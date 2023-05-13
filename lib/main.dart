1572bbc4db714e7843f1cfd32e336732a534a3da
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-05-13 06:37:55 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ejemplo de navegación',
+      title: 'Taller Flutter',
       initialRoute: '/',
       routes: {
         '/': (context) => PrimeraPantalla(),
